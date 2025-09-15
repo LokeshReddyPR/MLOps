0b5cab36f89d12ef0268d9e3d1e7d33e938c38e8
LokeshReddyPR <lpamredd@gmu.edu>
2025-09-15 09:27:48 -0400

    Creatng app.py

--- Flask/flask/app.py
+++ Flask/flask/app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__) #Creates an instance of flask class which will be you WSGI
-
+app = Flask(__name__)  # Creates an instance of flask class which will be you WSGI
 
 
 @app.route("/")
 def Welcome():
-    return("Welcome to the Flask COurse")
+    return "Welcome to the Flask COurse. THis should be a amazing course"
 
 
-if __name__=="__main__":
-    app.run()
+@app.route("/index")
+def index():
+    return "Welcome to the Flask COurse. THis should be a Index page"
 
 
-
-
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5001)
